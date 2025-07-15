2163dab949256ad5893b04f9143796210c919123
gleafy <greenleafy@yandex.ru>
2025-07-15 19:10:05 +0400

    контроллеры

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'home.html')
+
+def contacts_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        success = True
+    else:
+        success = False
+    return render(request, 'contacts.html', {'success': success})
